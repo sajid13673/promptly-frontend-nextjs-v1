edb956d4e44945fde89abd55ed678c6fa52fbfca
Sajid Ahamed <sajid13673@gmail.com>
2025-09-26 23:05:01 +0530

    refactor: improve message layout and responsive stylin

--- src/app/(site)/(chat)/[chatId]/page.tsx
+++ src/app/(site)/(chat)/[chatId]/page.tsx
@@ -1,49 +1,47 @@
 "use client";
-import { getConversationById } from '@/lib/api';
-import type { Conversation } from '@/types/Conversation';
-import { Message } from '@/types/Message';
-import React, { use, useEffect, useState } from 'react'
+import { getConversationById } from "@/lib/api";
+import type { Conversation } from "@/types/Conversation";
+import { Message } from "@/types/Message";
+import React, { use, useEffect, useState } from "react";
 
-function Conversation({params} : { params: Promise<{chatId: string}>}) {
-    const {chatId} = use(params)
-    const [conversation, setConversation] = useState<Conversation | null>(null)
-    console.log('conversation', chatId);
-    
-    const fetchConversation = async () => {
-      const res = await getConversationById(chatId);
-      setConversation(res.data)
-    }
+function Conversation({ params }: { params: Promise<{ chatId: string }> }) {
+  const { chatId } = use(params);
+  const [conversation, setConversation] = useState<Conversation | null>(null);
+  console.log("conversation", chatId);
 
+  const fetchConversation = async () => {
+    const res = await getConversationById(chatId);
+    setConversation(res.data);
+  };
 
-    useEffect(() => {
-      fetchConversation()
-    }, [])
-    
+  useEffect(() => {
+    fetchConversation();
+  }, []);
+
   return (
-    <div className='flex flex-col flex-1 items-end p-5'>
-      {conversation?.messages &&
-    conversation.messages.map((message: Message) => {
-      const isUser = message.role === 'USER';
-      return (
-        <div
-          key={message.id}
-          className={`flex ${isUser ? 'justify-end' : 'justify-start'}`}
-        >
-          <div
-            className={`text-lg p-2 rounded-md max-w-[75%] ${
-              isUser
-                ? 'bg-blue-100 text-blue-800 text-right'
-                : 'bg-gray-100 text-gray-800 text-left'
-            }`}
-          >
-            {message.message}
-          </div>
-        </div>
-      );
-    })}
-
+    <div className="flex flex-col items-center flex-1 gap-2 p-2">
+      <div className="w-full min-w-md lg:max-w-4xl text-white p-3">
+        {conversation?.messages &&
+          conversation.messages.map((message: Message) => {
+            const isUser = message.role === "USER";
+            return (
+              <div
+                key={message.id}
+                className={`flex ${isUser ? "justify-end" : "justify-start"}`}
+              >
+                <div
+                  className={`text-lg p-3 rounded-xl text-white ${
+                    isUser ? "bg-blue-600/40" : "mr-4"
+                  }`}
+                >
+                  {message.message}
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Conversation
+export default Conversation;
