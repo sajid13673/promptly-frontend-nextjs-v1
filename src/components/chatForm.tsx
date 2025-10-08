373630f27b461746fea96ca61c84ce4038e4bf44
Sajid Ahamed <sajid13673@gmail.com>
2025-10-08 22:40:04 +0530

    feat: add loading spinner to submit button

--- src/components/chatForm.tsx
+++ src/components/chatForm.tsx
@@ -2,15 +2,19 @@
 
 import { ArrowUpCircleIcon } from "@heroicons/react/16/solid";
 import { JSX, useState } from "react";
+import LoadingSpinner from "./loadingSpinner";
 
 type ChatFormProps = {
   onSend: (message: string) => void | Promise<void>;
 };
 function ChatForm({ onSend }: ChatFormProps): JSX.Element {
   const [message, setMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setLoading(true);
     await onSend(message);
+    setLoading(false);
     setMessage("");
   };
   return (
@@ -34,8 +38,18 @@ function ChatForm({ onSend }: ChatFormProps): JSX.Element {
           style={{ maxHeight: "200px" }}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button type="submit" className="cursor-pointer w-fit ml-auto mr-2">
-          <ArrowUpCircleIcon className="h-8 w-8 text-purple-200 " />
+        <button
+          type="submit"
+          disabled={loading}
+          className={`bg-purple-600 rounded-2xl cursor-pointer w-10 h-10 ml-auto mr-2 flex items-center justify-center ${
+            loading ? "opacity-80 cursor-not-allowed" : ""
+          }`}
+        >
+          {loading ? (
+            <LoadingSpinner size={1.5} color="purple-200" border={4} />
+          ) : (
+            <ArrowUpCircleIcon className="h-8 w-8 text-purple-200" />
+          )}
         </button>
       </div>
     </form>
